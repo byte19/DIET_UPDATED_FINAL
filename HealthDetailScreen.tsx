dfefd2c57185d52195d49f3c5dfb714a6dec06fb
Shashi Tarode <Shashi.Tarode@kal.com>
2024-03-28 22:47:31 +0530

    new design

--- HealthDetailScreen.tsx
+++ HealthDetailScreen.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, TextInput, ScrollView, StyleSheet } from 'react-native';
 import axios from 'axios';
 
+import COLORS from './src/conts/colors'
+import Button from './src/views/components/Button';
+import Input from './src/views/components/Input';
+import Loader from './src/views/components/Loader';
 const HealthDetailsScreen: React.FC = () => {
   const [hemoglobin, setHemoglobin] = useState<string>('');
   const [weight, setWeight] = useState<string>('');
@@ -9,6 +13,72 @@ const HealthDetailsScreen: React.FC = () => {
   const [sugar, setSugar] = useState<string>('');
   const [bloodPressure, setBloodPressure] = useState<string>('');
   const [foodSuggestions, setFoodSuggestions] = useState<string[]>([]);
+
+
+    const [inputs, setInputs] = React.useState({
+        email: '',
+        fullname: '',
+        phone: '',
+        password: '',
+    });
+    const [errors, setErrors] = React.useState({});
+    const [loading, setLoading] = React.useState(false);
+
+    const validate = () => {
+        Keyboard.dismiss();
+        let isValid = true;
+
+        if (!inputs.email) {
+            handleError('Please input email', 'email');
+            isValid = false;
+        } else if (!inputs.email.match(/\S+@\S+\.\S+/)) {
+            handleError('Please input a valid email', 'email');
+            isValid = false;
+        }
+
+        if (!inputs.fullname) {
+            handleError('Please input fullname', 'fullname');
+            isValid = false;
+        }
+
+        if (!inputs.phone) {
+            handleError('Please input phone number', 'phone');
+            isValid = false;
+        }
+
+        if (!inputs.password) {
+            handleError('Please input password', 'password');
+            isValid = false;
+        } else if (inputs.password.length < 5) {
+            handleError('Min password length of 5', 'password');
+            isValid = false;
+        }
+
+        if (isValid) {
+            register();
+        }
+    };
+
+    const register = () => {
+        setLoading(true);
+        setTimeout(() => {
+            try {
+                setLoading(false);
+                AsyncStorage.setItem('userData', JSON.stringify(inputs));
+                navigation.navigate('LoginScreen');
+            } catch (error) {
+                Alert.alert('Error', 'Something went wrong');
+            }
+        }, 3000);
+    };
+
+    const handleOnchange = (text, input) => {
+        setInputs(prevState => ({ ...prevState, [input]: text }));
+    };
+    const handleError = (error, input) => {
+        setErrors(prevState => ({ ...prevState, [input]: error }));
+    };
+
 
   const handleSubmission = async () => {
     const options = {
@@ -39,7 +109,68 @@ const HealthDetailsScreen: React.FC = () => {
   };
 
   return (
-    <ScrollView style={styles.container}>
+      <ScrollView style={styles.container}>
+          <Text style={{ color: COLORS.black, fontSize: 40, fontWeight: 'bold' }}>
+              Health Parameters
+          </Text>
+          <Text style={{ color: COLORS.grey, fontSize: 18, marginVertical: 10 }}>
+              Enter Your Details to Register
+          </Text>
+          <View style={{ marginVertical: 20 }}>
+              <Input
+                  keyboardType="numeric"
+                  onChangeText={text => handleOnchange(text, 'email')}
+                  onFocus={() => handleError(null, 'email')}
+                  iconName="email-outline"
+                  label="Height"
+                  placeholder="Enter your height"
+              />
+
+              <Input
+                  keyboardType="numeric"
+                  onChangeText={text => handleOnchange(text, 'fullname')}
+                  onFocus={() => handleError(null, 'fullname')}
+                  iconName="account-outline"
+                  label="Weight"
+                  placeholder="Enter your weight"
+              />
+
+              <Input
+                  keyboardType="numeric"
+                  onChangeText={text => handleOnchange(text, 'phone')}
+                  onFocus={() => handleError(null, 'phone')}
+                  iconName="phone-outline"
+                  label="Hemoglobin Level"
+                  placeholder="Enter your Hemoglobin Level"
+              />
+              <Input
+                  keyboardType="numeric"
+                  onChangeText={text => handleOnchange(text, 'password')}
+                  onFocus={() => handleError(null, 'password')}
+                  iconName="lock-outline"
+                  label="Sugar Level"
+                  placeholder="Enter your Sugar Level"
+              />
+              <Input
+                  keyboardType="numeric"
+                  onChangeText={text => handleOnchange(text, 'password')}
+                  onFocus={() => handleError(null, 'password')}
+                  iconName="lock-outline"
+                  label="Blood Pressure"
+                  placeholder="Enter your Blood pressure"
+              />
+              <Button title="Register"/>
+              <Text
+                  onPress={() => navigation.navigate('LoginScreen')}
+                  style={{
+                      color: COLORS.black,
+                      fontWeight: 'bold',
+                      textAlign: 'center',
+                      fontSize: 16,
+                  }}>
+                  Already have account ?Login
+              </Text>
+        </View>
       <Text>Height</Text>
       <TextInput
         style={styles.input}
